dc6d76f5c2ad90701e104c21983c011890e92e5e
Cash Myers <cmyers880@gmail.com>
2025-05-20 09:12:11 -0500

    feat: Standard error message pages as options instead of console.log(err).

---
+++ src/constants/AppConst.ts
@@ -0,0 +1,15 @@
+export const authRole = {
+  Admin: ["admin"],
+  User: ["user", "admin"],
+};
+
+export const defaultUser = {
+  uid: "john-alex",
+  displayName: "John Alex",
+  email: "demo@example.com",
+  token: "access-token",
+  role: "user",
+  photoURL: "/assets/images/A11.jpg",
+};
+export const allowMultiLanguage = import.meta.env.VITE__MULTILINGUAL === "true";
+export const initialUrl = import.meta.env.VITE__INITIAL_URL as string; // this url will open after login
